6c10f31b70f1102bc2a0eb47d5568bc4d7b7bce1
z18ista20 <z18ista20@Z00P1-13.SPSKNM>
2019-10-24 09:04:43 +0200

    Add project files.

---
+++ Open-Lab-03.5/MatchCaseInsensitive.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Open_Lab_03._5
+{
+    class MatchCaseInsensitive
+    {
+        public bool Match (string a, string b)
+        {
+            return a == b;
+        }
+    }
+}
---
+++ Open-Lab-03.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_03._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();     
+            string b = Console.ReadLine();
+            
+
+            MatchCaseInsensitive mci = new MatchCaseInsensitive();
+
+            Console.WriteLine(mci.Match(a.ToLower(), b.ToLower()));
+        }
+    }
+}
